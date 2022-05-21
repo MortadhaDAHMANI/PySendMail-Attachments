c773228217eb4a9bf0717005e6e6a774a15199e5
Mortadha DAHMANI <40170728+MortadhaDAHMANI@users.noreply.github.com>
2022-05-21 02:15:12 +0100

    Update support SSL and TLS

--- mailAttchments.py
+++ mailAttchments.py
@@ -48,7 +48,16 @@ message.attach(part)
 text = message.as_string()
 ##print(text)
 # Log in to server using secure context and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, text)
+
+try:
+    with smtplib.SMTP("ssl0.ovh.net", 587) as server:
+        server.starttls()
+        server.login(login_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+except :
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+    
+#'''
